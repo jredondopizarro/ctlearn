6f6646252fc3e98a6ef6f0100feb8b8d12a150ff
Juan Redondo Pizarro <jrp1996@hotmail.es>
2020-07-11 21:17:37 +0200

    Fix error

--- ctlearn/run_model.py
+++ ctlearn/run_model.py
@@ -322,7 +322,6 @@ def run_model_tf(config, mode="train", debug=False, log_to_file=False, multiple_
         else:
             t = tf.Variable(1.0) # no sirve para nada en este caso
 
-
         optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE,
                                              epsilon=EPSILON)
 
@@ -379,8 +378,7 @@ def run_model_tf(config, mode="train", debug=False, log_to_file=False, multiple_
                         kl_weight = 1 / num_training_examples * tf.minimum(1.0, kl_regularizer)
                     else:
                         kl_regularizer = t / (KL_ANNEALING * num_training_examples / batch_size)
-                        kl_weight = tf.cast(2 ** (training_steps_per_epoch - t) / (2 ** training_steps_per_epoch - 1),
-                                            dtype=tf.float64)
+                        kl_weight = 2**(-t)
                 else:
                     kl_regularizer = t / (KL_ANNEALING * num_training_examples / batch_size)
                     kl_weight = 1 / num_training_examples
